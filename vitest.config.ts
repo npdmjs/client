6dff46f310222a49e59cc6275b6466acf79e46ab
Aleksandr Kondratskii <aleksandr.kondratskii@macys.com>
2024-06-16 23:33:28 +0200

    test: add collecting code coverage for unit tests

--- vitest.config.ts
+++ vitest.config.ts
@@ -5,6 +5,7 @@ export default defineConfig({
     globals: true,
     include: ['src/**/__tests__/*.test.ts'],
     coverage: {
+      reporter: ['text', 'lcov'],
       exclude: ['**/index.ts'],
     },
   },
